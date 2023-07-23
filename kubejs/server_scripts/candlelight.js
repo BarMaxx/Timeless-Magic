1a2b808137dcf68e6a2bb821073fe6d9ff5d5038
BarMaxx <mbarakhtanov@mail.ru>
2023-07-23 18:51:28 +0300

    1.0.10
    - Изменены некотоые квесты (в основном текстуры)
    - Добавлено описание к некоторым квестам из главы "Истоки"
    - Изменен перевод мода iron chests (но он всё еще не рабочий)
    - Изменен рецепт из мода Let's Do Candlelight (он наконец-то работает)
    - Добавлены новые формы квестов, а также их текстуры и перевод
    - Изменено множество текстур из мода FTBQuests (в основном для GUI, но и также текстура книги квестов)

--- kubejs/server_scripts/candlelight.js
+++ kubejs/server_scripts/candlelight.js
@@ -1,20 +1,18 @@
 ServerEvents.recipes(event => {
-	
-	console.log('Hi! I'm a recipes downloader!')
 
 
 	event.remove({output: 'candlelight:cooking_pan'})
 	event.shaped(
-		Item.of('candlelight:cooking_pan'), 
-		[
-			' CC',
-			' CC',
-			'S  '
-		], 
-		{
-			S: 'minecraft:stick',
-			C: 'minecraft:copper_ingot'
-		}
+	  Item.of('candlelight:cooking_pan'), 
+	  [
+	    ' CC',
+		' CC',
+		'S  '
+	  ], 
+	  {
+		S: 'minecraft:stick',
+		C: 'minecraft:copper_ingot'
+	  }
 	)
 	
 	
