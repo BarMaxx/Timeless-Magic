5b190e174adf97f17eb0b0f707404d872d393847
BarMaxx <mbarakhtanov@mail.ru>
2023-07-27 23:01:34 +0300

    1.0.13
    - Изменены некоторые квесты
    - Изменены текстуры виджетов игры
    - Добавлены новые предметы и текстуры к ним
    - Добавлены переводы для некоторых модов

---
+++ kubejs/startup_scripts/timeless_magic.js
@@ -0,0 +1,33 @@
+StartupEvents.registry('item', event => {
+
+
+	event.create('display').displayName('Дисплей')
+	
+	event.create('panel_1').displayName('Панель 1')
+	
+	event.create('panel_2').displayName('Панель 2')
+	
+	event.create('panel_3').displayName('Панель 3')
+	
+	event.create('board').displayName('Плата')
+	
+	event.create('screen_1').displayName('Экран 1')
+	
+	event.create('screen_2').displayName('Экран 2')
+	
+	event.create('screen_3').displayName('Экран 3')
+	
+	event.create('hammer_handle').displayName('Рукоятка Молота')
+	
+	event.create('hammer_handle_part_1').displayName('Часть Рукоятки Молота 1')
+	
+	event.create('hammer_handle_part_2').displayName('Часть Рукоятки Молота 2')
+	
+	event.create('hammer_handle_part_3').displayName('Часть Рукоятки Молота 3')
+	
+	event.create('magical_thing').displayName('Магическая Штуковина')
+	
+	event.create('banglum_core').displayName('Банглум Ядро')
+	
+	
+})
