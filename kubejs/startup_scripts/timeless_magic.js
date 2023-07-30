445ef6d6014e7d3b2c7fd0c2eb51df0191466524
BarMaxx <mbarakhtanov@mail.ru>
2023-07-30 20:51:06 +0300

    1.0.15
    - Немного изменены некоторые скрипты
    - Немного изменены некоторые квесты

--- kubejs/startup_scripts/timeless_magic.js
+++ kubejs/startup_scripts/timeless_magic.js
@@ -3,31 +3,31 @@ StartupEvents.registry('item', event => {
 
 	event.create('display').displayName('Дисплей')
 	
-	event.create('panel_1').displayName('Панель 1')
+	event.create('panel_1').unstackable().displayName('Панель 1')
 	
-	event.create('panel_2').displayName('Панель 2')
+	event.create('panel_2').unstackable().displayName('Панель 2')
 	
-	event.create('panel_3').displayName('Панель 3')
+	event.create('panel_3').unstackable().displayName('Панель 3')
 	
 	event.create('board').displayName('Плата')
 	
-	event.create('screen_1').displayName('Экран 1')
+	event.create('screen_1').unstackable().displayName('Экран 1')
 	
-	event.create('screen_2').displayName('Экран 2')
+	event.create('screen_2').unstackable().displayName('Экран 2')
 	
-	event.create('screen_3').displayName('Экран 3')
+	event.create('screen_3').unstackable().displayName('Экран 3')
 	
-	event.create('hammer_handle').displayName('Рукоятка Молота')
+	event.create('hammer_handle').unstackable().displayName('Рукоятка Молота')
 	
-	event.create('hammer_handle_part_1').displayName('Часть Рукоятки Молота 1')
+	event.create('hammer_handle_part_1').unstackable().displayName('Часть Рукоятки Молота 1')
 	
-	event.create('hammer_handle_part_2').displayName('Часть Рукоятки Молота 2')
+	event.create('hammer_handle_part_2').unstackable().displayName('Часть Рукоятки Молота 2')
 	
-	event.create('hammer_handle_part_3').displayName('Часть Рукоятки Молота 3')
+	event.create('hammer_handle_part_3').unstackable().displayName('Часть Рукоятки Молота 3')
 	
 	event.create('magical_thing').displayName('Магическая Штуковина')
 	
-	event.create('banglum_core').displayName('Банглум Ядро')
+	event.create('banglum_core').unstackable().displayName('Банглум Ядро')
 	
 	
 })
