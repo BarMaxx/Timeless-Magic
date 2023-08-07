287400d74a2e4eff2376265bc825e6455f35bc56
BarMaxx <mbarakhtanov@mail.ru>
2023-08-07 20:32:15 +0300

    1.0.19
    - Немного изменены некоторые квесты
    - Изменено описание некоторых квестов
    - Изменены некоторые рецепты мода Wizards

--- kubejs/server_scripts/wizards.js
+++ kubejs/server_scripts/wizards.js
@@ -17,4 +17,196 @@ ServerEvents.recipes(event => {
 	)
 	
 	
+	event.remove({output: 'wizards:fire_robe_head'})
+	event.shaped(
+	  Item.of('wizards:fire_robe_head'), 
+	  [
+	    '  R',
+		'BRB',
+		'RFR'
+	  ], 
+	  {
+		R: 'minecraft:red_wool',
+		F: 'runes:fire_stone',
+		B: 'minecraft:blaze_powder'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:fire_robe_chest'})
+	event.shaped(
+	  Item.of('wizards:fire_robe_chest'), 
+	  [
+	    'B B',
+		'RFR',
+		'RRR'
+	  ], 
+	  {
+		R: 'minecraft:red_wool',
+		F: 'runes:fire_stone',
+		B: 'minecraft:blaze_powder'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:fire_robe_legs'})
+	event.shaped(
+	  Item.of('wizards:fire_robe_legs'), 
+	  [
+	    'BFB',
+		'R R',
+		'R R'
+	  ], 
+	  {
+		R: 'minecraft:red_wool',
+		F: 'runes:fire_stone',
+		B: 'minecraft:blaze_powder'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:fire_robe_feet'})
+	event.shaped(
+	  Item.of('wizards:fire_robe_feet'), 
+	  [
+	    'BFB',
+		'R R',
+		'   '
+	  ], 
+	  {
+		R: 'minecraft:red_wool',
+		F: 'runes:fire_stone',
+		B: 'minecraft:blaze_powder'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:arcane_robe_head'})
+	event.shaped(
+	  Item.of('wizards:arcane_robe_head'), 
+	  [
+	    '  P',
+		'EPE',
+		'PAP'
+	  ], 
+	  {
+		P: 'minecraft:purple_wool',
+		A: 'runes:arcane_stone',
+		E: 'minecraft:ender_pearl'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:arcane_robe_chest'})
+	event.shaped(
+	  Item.of('wizards:arcane_robe_chest'), 
+	  [
+	    'E E',
+		'PAP',
+		'PPP'
+	  ], 
+	  {
+		P: 'minecraft:purple_wool',
+		A: 'runes:arcane_stone',
+		E: 'minecraft:ender_pearl'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:arcane_robe_legs'})
+	event.shaped(
+	  Item.of('wizards:arcane_robe_legs'), 
+	  [
+	    'EAE',
+		'P P',
+		'P P'
+	  ], 
+	  {
+		P: 'minecraft:purple_wool',
+		A: 'runes:arcane_stone',
+		E: 'minecraft:ender_pearl'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:arcane_robe_feet'})
+	event.shaped(
+	  Item.of('wizards:arcane_robe_feet'), 
+	  [
+	    'EAE',
+		'P P',
+		'   '
+	  ], 
+	  {
+		P: 'minecraft:purple_wool',
+		A: 'runes:arcane_stone',
+		E: 'minecraft:ender_pearl'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:frost_robe_head'})
+	event.shaped(
+	  Item.of('wizards:frost_robe_head'), 
+	  [
+	    '  L',
+		'PLP',
+		'LFL'
+	  ], 
+	  {
+		L: 'minecraft:light_blue_wool',
+		F: 'runes:frost_stone',
+		P: 'minecraft:prismarine_shard'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:frost_robe_chest'})
+	event.shaped(
+	  Item.of('wizards:frost_robe_chest'), 
+	  [
+	    'P P',
+		'LFL',
+		'LLL'
+	  ], 
+	  {
+		L: 'minecraft:light_blue_wool',
+		F: 'runes:frost_stone',
+		P: 'minecraft:prismarine_shard'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:frost_robe_legs'})
+	event.shaped(
+	  Item.of('wizards:frost_robe_legs'), 
+	  [
+	    'PFP',
+		'L L',
+		'L L'
+	  ], 
+	  {
+		L: 'minecraft:light_blue_wool',
+		F: 'runes:frost_stone',
+		P: 'minecraft:prismarine_shard'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:frost_robe_feet'})
+	event.shaped(
+	  Item.of('wizards:frost_robe_feet'), 
+	  [
+	    'PFP',
+		'L L',
+		'   '
+	  ], 
+	  {
+		L: 'minecraft:light_blue_wool',
+		F: 'runes:frost_stone',
+		P: 'minecraft:prismarine_shard'
+	  }
+	)
+	
+	
 })
