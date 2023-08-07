593ef652d13c31c0d1c1810c9706bf57c1288a91
BarMaxx <mbarakhtanov@mail.ru>
2023-08-07 23:56:51 +0300

    1.0.20
    - Изменено описание некоторых квестов
    - Изменены некоторые квесты
    - Изменены некоторые рецепты из мода Wizards
    - Добавлена новая таблица наград
    - Добавлена новая структура для использования в квестах (Наземная агломерационная плита из Botania)

--- kubejs/server_scripts/wizards.js
+++ kubejs/server_scripts/wizards.js
@@ -209,4 +209,154 @@ ServerEvents.recipes(event => {
 	)
 	
 	
+	event.remove({output: 'wizards:wand_fire'})
+	event.shaped(
+	  Item.of('wizards:wand_fire'), 
+	  [
+	    '  P',
+		' N ',
+		'G  '
+	  ], 
+	  {
+		P: 'minecraft:gunpowder',
+		N: 'wizards:wand_novice',
+		G: 'minecraft:gold_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_fire'})
+	event.shaped(
+	  Item.of('wizards:staff_fire'), 
+	  [
+	    ' NB',
+		' FN',
+		'G  '
+	  ], 
+	  {
+		N: 'minecraft:nether_brick',
+		F: 'wizards:wand_fire',
+		B: 'minecraft:blaze_powder',
+		G: 'minecraft:gold_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_ruby_fire'})
+	event.shaped(
+	  Item.of('wizards:staff_ruby_fire'), 
+	  [
+	    ' RB',
+		' FR',
+		'C  '
+	  ], 
+	  {
+		R: 'betternether:nether_ruby',
+		F: 'wizards:staff_fire',
+		B: 'minecraft:blaze_powder',
+		C: 'betternether:cincinnasite_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:wand_arcane'})
+	event.shaped(
+	  Item.of('wizards:wand_arcane'), 
+	  [
+	    '  A',
+		' N ',
+		'G  '
+	  ], 
+	  {
+		A: '#spectrum:gemstone_shards',
+		N: 'wizards:wand_novice',
+		G: 'minecraft:gold_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_arcane'})
+	event.shaped(
+	  Item.of('wizards:staff_arcane'), 
+	  [
+	    ' AE',
+		' WA',
+		'G  '
+	  ], 
+	  {
+		A: '#spectrum:gemstone_shards',
+		W: 'wizards:wand_arcane',
+		E: 'minecraft:ender_pearl',
+		G: 'minecraft:gold_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_crystal_arcane'})
+	event.shaped(
+	  Item.of('wizards:staff_crystal_arcane'), 
+	  [
+	    ' CE',
+		' SC',
+		'A  '
+	  ], 
+	  {
+		C: 'betterend:crystal_shards',
+		S: 'wizards:staff_arcane',
+		E: 'minecraft:ender_pearl',
+		A: 'betterend:aeternium_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:wand_frost'})
+	event.shaped(
+	  Item.of('wizards:wand_frost'), 
+	  [
+	    '  S',
+		' N ',
+		'I  '
+	  ], 
+	  {
+		S: 'minecraft:snowball',
+		N: 'wizards:wand_novice',
+		I: 'minecraft:iron_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_frost'})
+	event.shaped(
+	  Item.of('wizards:staff_frost'), 
+	  [
+	    ' SP',
+		' FS',
+		'I  '
+	  ], 
+	  {
+		S: 'minecraft:snowball',
+		F: 'wizards:wand_frost',
+		P: 'minecraft:prismarine_crystals',
+		I: 'minecraft:iron_ingot'
+	  }
+	)
+	
+	
+	event.remove({output: 'wizards:staff_smaragdant_frost'})
+	event.shaped(
+	  Item.of('wizards:staff_smaragdant_frost'), 
+	  [
+	    ' SP',
+		' FS',
+		'A  '
+	  ], 
+	  {
+		S: 'betterend:smaragdant_crystal_shard',
+		F: 'wizards:staff_frost',
+		P: 'minecraft:prismarine_crystals',
+		A: 'betterend:aeternium_ingot'
+	  }
+	)
+	
+	
 })
